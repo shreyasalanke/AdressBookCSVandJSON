ed0ab5cafea2bbb7209c8ff9c41573384e03f2d5
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-23 00:27:03 +0530

    Initial: Welcome to Address book for CSV and Json file

---
+++ lib/src/main/java/com/bridgelabz/addressbookcsvandjson/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbookcsvandjson;
+
+public class AddressBookMain 
+{
+  public static void main(String[] args)
+   {
+	System.out.println("Welcome to Address Book for CSV and Json File ");
+   }
+}
